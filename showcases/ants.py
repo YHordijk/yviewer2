e5f149bd651c709f6147ba7e3c8db61879edd125
YHordijk <42876712+YHordijk@users.noreply.github.com>
2023-08-27 23:06:56 +0200

    Added diffusion

--- showcases/ants.py
+++ showcases/ants.py
@@ -9,8 +9,8 @@ screen.set_size(600, 600)
 
 # setup
 Nants = 4_000
-# ant_pos = np.random.rand(Nants, 2) * screen.settings.size
-ant_pos = np.array([screen.middle] * Nants)
+ant_pos = np.random.rand(Nants, 2) * screen.settings.size
+# ant_pos = np.array([screen.middle] * Nants)
 # ant_ang = np.random.rand(Nants, 1) * 2 * np.pi
 ant_ang = np.linspace(0, 2*np.pi, Nants).reshape(-1, 1)
 
@@ -24,13 +24,25 @@ cols2 = np.array([(255, 0, 0)] * Nants)
 
 antenna_length = 20
 antenna_angle = 45 * np.pi/180
-turning_speed = 3
+turning_speed = 2
+
+neighbours = [
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (0, -1),
+    (-1, -1),
+    (-1, 0),
+    ]
+
 
 while loop.runs():
     screen.clear()
 
     direc = np.hstack((np.cos(ant_ang), np.sin(ant_ang)))
-    ant_pos += direc * 1
+    ant_pos += direc * 2
     screen.draw_matrix(pheromones/pheromones.max())
 
     antenna1 = np.mod(ant_pos + np.hstack((np.cos(ant_ang+antenna_angle), np.sin(ant_ang+.4)))*antenna_length, screen.settings.size)
@@ -45,8 +57,15 @@ while loop.runs():
 
     ant_pos = np.mod(ant_pos, screen.settings.size)
     pheromones = pheromones * (1 - .1 * loop.state.delta_time)
+
     pheromones[ant_pos.astype(int)[:, 0], ant_pos.astype(int)[:, 1]] += 1
     screen.draw_pixels(ant_pos, cols)
+
+    # spread pheromones:
+    diffuse = np.zeros_like(pheromones)
+    for neighbour in neighbours:
+        diffuse += np.roll(pheromones, neighbour, (1, 0))/9
+    pheromones = diffuse
 
     if yviewer2.inputs.keys.space:
         pheromones = np.zeros(screen.settings.size)
